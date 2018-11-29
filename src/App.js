5b7c3c6bd55c40ec37fe4e16f13dcc3a8d0c4416
Praneeth <npraneeth.arsenal@gmail.com>
2018-11-29 21:40:54 +0530

    Ok

--- src/App.js
+++ src/App.js
@@ -10,22 +10,33 @@ class App extends Component {
             ]
   }
   
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log("Was Clicked");
     //this.state.persons[0].name = "N Praneeth";
     this.setState({persons:[
-                    {name:"N Praneeth",age:22},
+                    {name:newName,age:22},
                     {name:"Anand",age:22}
                    ]});
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({persons:[
+                    {name:"Praneeth",age:22},
+                    {name:event.target.value,age:22}
+                  ]});
   }
 
   render() {
     return (
       <div className="App">
         <h1> My React App!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+        <button onClick={this.switchNameHandler.bind(this,"Narasimha Praneeth")}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person 
+          change={this.nameChangedHandler}
+          click={this.switchNameHandler.bind(this,"N Praneeth")}
+          name={this.state.persons[1].name} age={this.state.persons[1].age}>
            My Hobby is Football
         </Person>
       </div>
